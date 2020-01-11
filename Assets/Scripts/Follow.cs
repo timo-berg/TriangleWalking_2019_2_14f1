e4e482a38c3f5e212bb665b139bccf9daaa0708e
tadeseus <timotheus.berg@gmx.de>
2020-01-11 16:41:39 +0100

    HMD Message

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+
+public class Follow : MonoBehaviour
+{
+	#region fields
+
+		public Transform target;
+		public float positionSmooth = 0.02f;
+
+		Vector3 vrOffset = new Vector3(0, 0, 1.2f);
+		Vector3 desktopOffset = new Vector3(0, 0.5f, 2f);
+
+	#endregion
+
+
+
+	public void AppearAtTargetPos()
+	{
+		transform.position = target.TransformPoint(ExperimentManager.Instance.isVR ? vrOffset : desktopOffset);
+		transform.rotation = target.rotation;
+	}
+
+
+
+
+	void LateUpdate()
+	{
+		Vector3 targetPos = target.TransformPoint(ExperimentManager.Instance.isVR ? vrOffset : desktopOffset);
+		Quaternion targetQ = target.rotation;
+
+		if ( ExperimentManager.Instance.isVR )
+		{
+			transform.position = Vector3.Lerp(transform.position, targetPos, positionSmooth);
+			//transform.rotation = Quaternion.Slerp(transform.rotation, targetQ, 0.1f);
+			transform.rotation = targetQ;
+		}
+		else
+		{
+			transform.position = targetPos;
+			transform.rotation = targetQ;
+		}
+
+	}
+
+
+
+	void Start()
+	{
+		if ( target == null )
+			target = Camera.main.transform;
+	}
+
+}
---
+++ Assets/Scripts/HMDMessageManager.cs
@@ -0,0 +1,97 @@
+﻿using TMPro;
+using UnityEngine;
+
+
+
+public class HMDMessageManager : Singleton<HMDMessageManager>
+{
+	#region fields
+
+		public TextMeshProUGUI messageText;
+		public GameObject canvas;
+
+		public Color normalColor;
+		public Color warningColor;
+		public Color errorColor;
+
+		Follow follow;
+		float targetShowDuration;
+		float showingTime;
+		bool isShowingText;
+
+	#endregion
+
+
+
+	protected override void Awake()
+	{	
+		base.Awake();
+
+		normalColor = messageText.color;
+
+		follow = GetComponent<Follow>();
+
+		HideMessage();
+	}
+
+
+	
+	public void HideMessage()
+	{
+		canvas.SetActive(false);
+		messageText.text = "";
+		isShowingText = false;
+	}
+
+
+
+	public void ShowError(string text, float duration = -1)
+	{
+		ShowText(text, duration, errorColor);
+	}
+
+
+
+	public void ShowMessage(string text, float duration = -1)
+	{
+		// print("\tOnScreen: " + text.Replace('\n', ' '));
+
+		ShowText(text, duration, normalColor);
+	}
+
+
+
+	public void ShowWarning(string text, float duration = -1)
+	{
+		ShowText(text, duration, warningColor);
+	}
+
+
+
+	void ShowText(string text, float duration, Color color)
+	{
+		if ( false == isShowingText )
+			follow.AppearAtTargetPos();
+
+		targetShowDuration = duration;
+		showingTime = 0;
+		isShowingText = true;
+
+		canvas.SetActive(true);
+		messageText.text = text;
+		messageText.color = color;
+	}
+
+
+
+	void Update()
+	{
+		if ( false == isShowingText )
+			return;
+	
+		showingTime += Time.deltaTime;
+		if ( targetShowDuration > 0 && showingTime > targetShowDuration )
+			HideMessage();
+	}
+
+}
